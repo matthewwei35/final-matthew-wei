b2ace390d5005c9f6f3f0a5941fcc62dc718a57c
Matthew Wei <matthewwei35@gmail.com>
2021-07-13 15:45:42 -0700

    feat(final): complete challenges 4/5

--- src/StarWars.js
+++ src/StarWars.js
@@ -1,17 +1,47 @@
 import React from 'react';
 import { useState } from 'react';
+import StarWarsDisplay from './StarWarsDisplay';
 
 function StarWars() {
   const [id, setId] = useState(1);
+  const [data, setData] = useState(null);
+
+  async function getCharacter(characterId) {
+    try {
+      const res = await fetch(`https://swapi.dev/api/people/${characterId}/`);
+      const json = await res.json();
+
+      const name = json.name;
+      const height = json.height;
+      const mass = json.mass;
+      const hairColor = json.hair_color;
+      const eyeColor = json.eye_color;
+
+      setData({
+        name,
+        height,
+        mass,
+        hairColor,
+        eyeColor
+      });
+    } catch(err) {
+      console.log(err);
+    }
+  }
 
   return (
     <div className="StarWars">
-      <input
-        onChange={(e) => setId(e.target.value)}
-        value={id}
-        placeholder={'Character Id'}
-      />
-      <button>Submit</button>
+      <form onSubmit={e => {
+        e.preventDefault();
+      }}>
+        <input
+          onChange={(e) => setId(e.target.value)}
+          value={id}
+          placeholder={'Character Id'}
+        />
+        <button onClick={() => getCharacter(id)}>Submit</button>
+      </form>
+      {data && <StarWarsDisplay {...data} />}
     </div>
   );
 }
---
+++ src/StarWarsDisplay.js
@@ -0,0 +1,15 @@
+function StarWarsDisplay(props) {
+  const { name, height, mass, hairColor, eyeColor } = props;
+  
+  return (
+    <div className="StarWarsDisplay">
+      <h2>{name}</h2>
+      <p>Height: {height}</p>
+      <p>Mass: {mass}</p>
+      <p>Hair Color: {hairColor}</p>
+      <p>Eye Color: {eyeColor}</p>
+    </div>
+  );
+}
+
+export default StarWarsDisplay;
